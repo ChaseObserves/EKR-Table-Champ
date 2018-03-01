4caa5bc1e8c567780193ce8afd7d47674f46f8da
Chase Adams <chaseadams@admins-MacBook-Pro-3.local>
2018-03-01 14:16:50 -0700

    first commit

---
+++ js/firebase-key.js
@@ -0,0 +1,5 @@
+var cs = { 
+  "apiKey": "AIzaSyAfL4eBodNfS5XRU51S2LkbXNtKcNLFyWE", 
+  "authDomain": "ekr-table-champ.firebaseapp.com", 
+  "databaseURL": "https://ekr-table-champ.firebaseio.com" 
+};
